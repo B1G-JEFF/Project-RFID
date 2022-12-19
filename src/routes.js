3b60e723a1f217fa39d2ccc70a4b68cf4a59c4da
B1G-JEFF <jf250292@gmail.com>
2022-12-19 13:39:48 -0300

    .

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ const router = Router();
 router.post("/user", UserControler.newUser);
 router.get("/users", UserControler.findAllUser);
 router.get("/users/:id", UserControler.findUser);
-router.put("/users/:id", UserControler.updateUser);
+router.put("/users/:id", UserControler.updateUser); 
 router.delete("/users/:id", UserControler.deleteUser);
 // rotas para criaçao e edicao dos cartoes
 router.post("/card/user/:id", CardControler.newCard)
@@ -16,5 +16,5 @@ router.get("/cards", CardControler.findAllCards);
 router.get("/card/:id", CardControler.findCard);
 router.put("/cards/:id", CardControler.updateCard);
 router.delete("/cards/:id", CardControler.deleteCard);
-
+// sdfdfss
 export { router };
